6c6d4e03495acadceb1b6fbf68ee076d820b714d
Rafael Cavalcanti <rfontes86@gmail.com>
2026-06-10 22:59:53 +0100

    updated

---
+++ exemplo_dicionario.py
@@ -0,0 +1,13 @@
+produto_01: dict = {
+    "nome": "Sapato",
+    "quantidade": 39,
+    "preço": 10.38,
+    "disponibilidade": True
+}
+
+produto_02: dict = {
+    "nome": "televisão",
+    "quantidade": 5,
+    "preço": 1500.00,
+    "disponibilidade": False
+}
