829caf1902bb5a55802546c9dc4c39cfeb8bd842
Rafael Cavalcanti <rfontes86@gmail.com>
2026-06-10 22:51:52 +0100

    updated

---
+++ exemplo_lista.py
@@ -0,0 +1,13 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+produtos.pop()
+produtos.pop()
+
+print (produtos)
